672b193512a923639858b8b30ded6ad75ce2b64f
cdowney086 <61474093+cdowney086@users.noreply.github.com>
2020-03-01 16:37:16 -0500

    Create DowneyFrankensteinsBookshelf.php
    Created for H547 spring 2020 week 8 assignment.

---
+++ DowneyFrankensteinsBookshelf.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Frankenstein's Bookshelf</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+<p>This essay will explore the literary works named and quoted from in Mary Shelley's <i>Frankenstein</i>, in particular John Milton's <i>Paradise Lost</i> and Samuel Taylor Coleridge's "Rime of the Ancient Mariner."
+This essay will explore the influence of the works on the novel in terms of themes and content. Multimodal components will include artwork and a virtual bookshelf of digitized works available on Archive.org or Hathitrust.</p>
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
